538c694faf86552d9bc3c4611004a4ee6ca5b513
aljuarizmi <aljuarizmi@gmail.com>
2025-01-28 10:27:45 -0500

    Nueva app empleados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { Empleado } from './empleado.model';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'app-empleados';
+  titulo = 'Listado de Empleados';
+  empleados:Empleado[]=[
+    new Empleado("Juan","Perez","Presidente",7500),
+    new Empleado("Luis","Guerra","Directora",5500),
+    new Empleado("Ana","Lopez","Jefa Sección",3500),
+    new Empleado("Lucía","García","Administrativo",2500),
+  ];
 }
