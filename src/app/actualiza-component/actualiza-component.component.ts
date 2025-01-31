317286959093aa10c77539ed52013975cfccd0cf
aljuarizmi <aljuarizmi@gmail.com>
2025-01-31 12:53:31 -0500

    Se agregan cambios usando la directiva ngIf para ocultar o mostrar los botones de modificar/eliminar. Referencia: pildorasinformaticas, Routing VII video 30

--- src/app/actualiza-component/actualiza-component.component.ts
+++ src/app/actualiza-component/actualiza-component.component.ts
@@ -4,16 +4,29 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { ServicioEmpleadosService } from '../servicio-empleados.service';
 import { EmpleadosService } from '../empleados.service';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-actualiza-component',
-  imports: [FormsModule],
+  imports: [FormsModule,CommonModule],
   templateUrl: './actualiza-component.component.html',
   styleUrl: './actualiza-component.component.css'
 })
 export class ActualizaComponentComponent {
+  accion!: number;
+  mostrarActualiza:boolean=true;
+  mostrarElimina:boolean=true;
   constructor(private router:Router,private miServicio:ServicioEmpleadosService,private empleadosService:EmpleadosService, private route:ActivatedRoute){
     this.empleados=this.empleadosService.empleados;
+    this.accion=parseInt(this.route.snapshot.queryParams['accion']);
+    if(this.accion==1){
+      this.mostrarActualiza=true;
+      this.mostrarElimina=false;
+    }
+    if(this.accion==2){
+      this.mostrarActualiza=false;
+      this.mostrarElimina=true;
+    }
     this.indice=this.route.snapshot.params['id'];
     let empleado:Empleado=this.empleadosService.encontrarEmpleado(this.indice);
     this.cuadroNombre=empleado.nombre;
